7e71f7a6136b5d891d3be9a09bcbb19aaf3e0387
“jiayun” <jiayun_work@163.com>
2026-08-22 00:26:27 +0800

    fix: propagate non-positive module error instead of dropping it

--- internal/gears/moduledrop.go
+++ internal/gears/moduledrop.go
@@ -1,12 +1,12 @@
 package gears
 
 func dropModule(err error) error {
-	if err != nil {
-		return nil
-	}
 	return err
 }
 
 func commitModule(err error) error {
+	if err == nil {
+		return nil
+	}
 	return dropModule(err)
 }
